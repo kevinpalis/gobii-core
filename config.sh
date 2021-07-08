2ffebde218260ace5b4c6ba4d98a02b794ff1b59
Kevin Palis <kdp44@cornell.edu>
2021-07-07 23:28:48 -0400

    It finally fookin builds. Took only forever.

---
+++ config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#author: Kevin Palis <kdp44@cornell.edu>, John Palis <johnv.palis@gmail.com>
+
+#TODO: Figure out what needs to happen on runtime - can be anything from setting up cronjobs (for standalone) to simply organizing directories (ex. where the jar files are)
+/bin/bash
